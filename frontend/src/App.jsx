ffb6855a92155925b39f97526dd4249050c408fc
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-01-23 18:03:02 +0530

    Update routing logic for single route change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       />
       <Route
         path="/signup"
-        element={!userData ? <SignUp /> : <Navigate to={"/customize"} />}
+        element={!userData ? <SignUp /> : <Navigate to={"/"} />}
       />
       <Route
         path="/signin"
